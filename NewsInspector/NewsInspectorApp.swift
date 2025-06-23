705149c6ebc06d3775c7273f7afb4827cde9cae3
Alexander Suhodolov <asuhodolov@appclose.com>
2025-06-23 14:39:48 +0200

    Initial Commit

---
+++ NewsInspector/NewsInspectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsInspectorApp.swift
+//  NewsInspector
+//
+//  Created by Alexander Suhodolov on 23/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct NewsInspectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
